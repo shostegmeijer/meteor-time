cda728b97233273b2dd88256d0258d45d511b5d7
Guyon <guyon.cornelissen@ah.nl>
2024-12-11 14:33:22 +0100

    fix hook

--- app/src/hello/HelloMeteors.tsx
+++ app/src/hello/HelloMeteors.tsx
@@ -1,21 +1,26 @@
-import { MeteorInformation } from "../meteor-information/MeteorInformation";
-import { useLatestMeteorShower } from "../hooks/useLatestMeteorShower";
-import { useCountdown } from "../hooks/calculateCountdown";
-import MeteorParticleEngine from "../meteors/Meteors";
+import { MeteorInformation } from '../meteor-information/MeteorInformation';
+import { useLatestMeteorShower } from '../hooks/useLatestMeteorShower';
+import { useCountdown } from '../hooks/calculateCountdown';
+import MeteorParticleEngine from '../meteors/Meteors';
 
 export const HelloMeteors = () => {
-    const shower = useLatestMeteorShower();
+    const { upcomingShower: shower, upcomingShowerDetails } = useLatestMeteorShower();
     const timeTillShower = useCountdown(shower?.dateTime);
 
     return (
         <>
             <div className="w-full h-screen flex items-center justify-center flex-col gap-4 relative">
                 <h1 className="flex text-3xl font-medium ">Next meteor shower is in:</h1>
-                {timeTillShower?.seconds ? <span className="text-5xl font-black">{timeTillShower?.days || 0} days {timeTillShower?.hours || 0} hours {timeTillShower?.minutes || 0} minutes {timeTillShower?.seconds || 0} seconds</span> : null}
+                {timeTillShower?.seconds ? (
+                    <span className="text-5xl font-black">
+                        {timeTillShower?.days || 0} days {timeTillShower?.hours || 0} hours{' '}
+                        {timeTillShower?.minutes || 0} minutes {timeTillShower?.seconds || 0}{' '}
+                        seconds
+                    </span>
+                ) : null}
                 {shower?.description}
                 <MeteorInformation />
             </div>
-
         </>
     );
 };
--- app/src/hooks/useLatestMeteorShower.ts
+++ app/src/hooks/useLatestMeteorShower.ts
@@ -22,6 +22,6 @@ export const useLatestMeteorShower = () => {
     getUpcomingShower();
   }, [fetchShowers]);
 
-  return { ...upcomingShower, ...upcomingShowerDetails };
+  return { upcomingShower, upcomingShowerDetails };
 };
 
